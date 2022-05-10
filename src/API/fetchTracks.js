df70358efda6f94d3ca39fcab63690d5e9c7df1e
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2022-05-10 17:04:36 -0400

    refactor artist call a bit

--- src/API/fetchTracks.js
+++ src/API/fetchTracks.js
@@ -24,73 +24,41 @@ export const getTracksFromArtist = async (artist) => {
   const artistObj = resultsJson.artists.items[0];
   const artistId = artistObj.id;
   // get the artist albums id
-  const albumIds = [];
-  const artistAlbumsUrl = new URL(
+  let artistAlbumsUrl = new URL(
     `https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album,single`
   );
-  const artistAlbums = await fetch(artistAlbumsUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  let artistAlbumsJson = await artistAlbums.json();
-  console.log(artistAlbumsJson);
-  artistAlbumsJson.items.forEach((album) => {
-    albumIds.push(album.id);
-  });
-  // get the tracks of each album
-  const getSeveralAlbumsUrl = new URL(
-    `	https://api.spotify.com/v1/albums?ids=${albumIds.join(",")}`
-  );
-  const getSeveralAlbumsResponse = await fetch(getSeveralAlbumsUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const getSeveralAlbumsJSON = await getSeveralAlbumsResponse.json();
-  console.log(getSeveralAlbumsJSON);
-  getSeveralAlbumsJSON.albums.forEach((album) => {
-    album.tracks.items.forEach((track) => tracks.push(track));
-  });
-
-  // If there are more albums, fetch them
-  // TODO: Wrap this and the above code fetching the first page of albums in a do while loop
-  let next = artistAlbumsJson.next;
-  while (next) {
-    const nextAlbumsIds = [];
-    const nextPage = await fetch(next, {
+  do {
+    const albumIds = [];
+    const artistAlbums = await fetch(artistAlbumsUrl, {
       method: "GET",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Bearer ${token}`,
       },
     });
-    const nextPageJson = await nextPage.json();
-    nextPageJson.items.forEach((album) => {
-      nextAlbumsIds.push(album.id);
+    const artistAlbumsJson = await artistAlbums.json();
+    artistAlbumsJson.items.forEach((album) => {
+      albumIds.push(album.id);
     });
     // get the tracks of each album
-    const getSeveralAlbumsUrlNext = new URL(
-      `	https://api.spotify.com/v1/albums?ids=${nextAlbumsIds.join(",")}`
+    const getSeveralAlbumsUrl = new URL(
+      `	https://api.spotify.com/v1/albums?ids=${albumIds.join(",")}`
     );
-    const getSeveralAlbumsResponseNext = await fetch(getSeveralAlbumsUrlNext, {
+    const getSeveralAlbumsResponse = await fetch(getSeveralAlbumsUrl, {
       method: "GET",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Bearer ${token}`,
       },
     });
-    const getSeveralAlbumsJSONNext = await getSeveralAlbumsResponseNext.json();
-    getSeveralAlbumsJSONNext.albums.forEach((album) => {
-      // TODO: Consider trimming down on the object that we push to the tracks array, also add the album name of the track
-      album.tracks.items.forEach((track) => tracks.push(track));
+    const getSeveralAlbumsJSON = await getSeveralAlbumsResponse.json();
+    getSeveralAlbumsJSON.albums.forEach((album) => {
+      album.tracks.items.forEach((track) =>
+        tracks.push({ ...track, album: album })
+      );
     });
-    next = nextPageJson.next;
-  }
+    artistAlbumsUrl = artistAlbumsJson.next;
+  } while (artistAlbumsUrl);
   console.log(tracks);
   return tracks;
 };
