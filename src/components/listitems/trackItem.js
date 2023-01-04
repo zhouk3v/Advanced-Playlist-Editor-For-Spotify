f47769ea4e04f5efab2aee9a92e04f514b067add
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2023-01-04 00:32:18 -0500

    Add links to track name

--- src/components/listitems/trackItem.js
+++ src/components/listitems/trackItem.js
@@ -3,6 +3,7 @@ import "./css/trackItem.css";
 
 export const TrackItem = ({ index, track }) => {
   let artistsNames = track.artists[0].name;
+  console.log(track);
   track.artists.forEach((artist, index) => {
     if (index > 0) {
       artistsNames = artistsNames + `, ${artist.name}`;
@@ -11,7 +12,11 @@ export const TrackItem = ({ index, track }) => {
   return (
     <tr className="track">
       <td className="index">{index}</td>
-      <td className="name">{track.name}</td>
+      <td className="name">
+        <a href={track.external_urls.spotify} target="_blank" rel="noreferrer">
+          {track.name}
+        </a>
+      </td>
       <td className="album">{track.album.name}</td>
       <td className="artist">{artistsNames}</td>
     </tr>
