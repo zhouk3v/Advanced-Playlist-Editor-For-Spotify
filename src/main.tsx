5ac357f57b72773b5a08a5bb4f043d7264223e9d
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2024-03-22 21:07:18 -0400

    readd AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import { AuthContextProvider } from "./context/auth-context.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
